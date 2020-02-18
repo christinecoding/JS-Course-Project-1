771db9db8bc08ef4b2f4c95c353c9f70cb4dbfff
christinecoding1 <60949965+christinecoding1@users.noreply.github.com>
2020-02-18 15:10:32 -0500

    Completed Project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # JS Course Project 1
  First JavaScript project
+
+This is a web application that displays random quotes each time a button is clicked
--- js/script.js
+++ js/script.js
@@ -1,27 +1,10 @@
-/******************************************
-Treehouse FSJS Techdegree:
-project 1 - A Random Quote Generator
-******************************************/
 
-// For assistance: 
-  // Check the "Project Resources" section of the project instructions
-  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
-
-/*** 
- * `quotes` array 
-***/
-
-// All objects have quote and source properties
-// At least one object has citation property
-// At least one object has year property
-
-//Array with 8 quotes and their sources. One quote has a 'citation' and 'year' property
-
+//Array of quote objects. All quote objects have a 'quote' and 'source' property and some have an additional 'citation' and/or 'year' property.
 let quotes = [
   { 
     quote: "Life is what happens when you’re busy making other plans.",
     source: "John Lennon",
-    citation: "'Beautiful Boy' (song)",
+    citation: "Beautiful Boy",
     year: "1980"
   },
   { 
@@ -53,46 +36,28 @@ let quotes = [
   }
 ]; 
 
-/***
- * `getRandomQuote` function
-***/
-
-//Function that pulls a random quote object from the quotes array everytime it is called or the page is refreshed
+//Function that pulls a random quote object from the quotes array 
 let getRandomQuote = () => {
-  return quotes[(Math.floor(Math.random() * 9))];
+  return quotes[(Math.floor(Math.random() * (quotes.length)))];
 };
 
-/***
- * `printQuote` function
-***/
-let printQuote = (){
+/* Function that uses the getRandomQuote function to build a concatenated string with HTML and quote properties and 
+then displays the resulting string in the browser */
+let printQuote = () => {
+  let randomQuote = getRandomQuote();
+  let message = "";
+  message+="<p class='quote'>" + randomQuote.quote + "</p>";
+  message+="<p class='source'>" + randomQuote.source;
 
+  if (randomQuote.hasOwnProperty('citation')){
+    message+="<span class='citation'>" + randomQuote.citation + "</span>";
+  }
+  if (randomQuote.hasOwnProperty('year')){
+    message+="<span class='year'>" + randomQuote.year + "</span>";
+  }
+  message+="</p>";
+  document.getElementById('quote-box').innerHTML = message; 
 };
-/*
-Needs work if any of the following are true:
 
-printQuote function:
-is not named printQuote
-does not call the getRandomQuote function
-does not print a quote and a source property with every quote
-does not print a citation property with at least one quote
-does not print a year property with at least one quote
-printed quotes do not match the format, layout or styles of the example quote in the index.html file
-Meets expectations if all of the following are true:
-
-printQuote function:
-is named printQuote
-calls the getRandomQuote function
-prints a quote and a source property with every quote
-prints a citation property with at least one quote
-prints a year property with at least one quote
-printed quotes match the format, layout and styles of the example quote in the index.html file */
-
-document.write()
-
-/***
- * click event listener for the print quote button
- * DO NOT CHANGE THE CODE BELOW!!
-***/
-
+//Click event listener that calls the printQuote function everytime the 'load-quote' button is clicked
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
